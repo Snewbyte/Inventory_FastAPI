cd2b8d3951d41733d3bfb97f92c9ab8bfdbd89f7
Sam <newboldsamuel@gmail.com>
2026-03-15 13:34:44 -0500

    Got first get_products function working

--- main.py
+++ main.py
@@ -13,8 +13,30 @@ app = FastAPI()
 
 @app.get("/product") #product endpoint
 def get_products(product_price: float, product_type: Union[str, None] = None, product_name: Union[str, None] = None):
-    return ["Hello World"]
+    results = []
+
+    for product in Products:
+        if product["Price"] != product_price: #if prices don't match then skip
+            continue
+
+        if product_type is not None and product["Type"] != product_type:  #if product type isn't none and doesn't match then skip
+            continue
+
+        if product_name is not None and product["Name"] != product_name:  #if product name isn't none and doesn't match then skip
+            continue
+
+        results.append(product) #put results in list
+
+    return results #return list
+
+
+
 
 @app.get("/product/price")
 def get_all_products_price_range(min_price: float, max_price: float):
-    return ["Hello World"]
+    results = []
+
+    for product in Products:
+        results.append(product)
+
+    return results
