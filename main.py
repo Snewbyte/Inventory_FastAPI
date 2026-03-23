86496fa55329b8f9c691382d3991c00b0a74cfbc
Sam <newboldsamuel@gmail.com>
2026-03-23 12:22:23 -0500

    Part 3b - POST endpoint product/mod can modify or create products in dictionary

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ Products = [{'ID': 0, 'Name': 'Apple', 'Price': 4.99, 'Type': 'Fruit'},
 #uvicorn main:app --reload
 
 from fastapi import FastAPI
-from typing import Union
+#from typing import Union
 from pydantic import BaseModel
 
 app = FastAPI(title = "Module 3 API",
               version = "0.0.2",
               contact = {"name": "Samuel Newbold", "email": "srnewbold17955@mail.mccneb.edu"},
-              description = "Introducing POST and swagger to API for assignment 3")
+              description = "Introducing POST and swagger to API for assignment 3"
+              )
 
 
 class Product(BaseModel):
@@ -27,3 +28,22 @@ class Product(BaseModel):
 @app.get("/products")
 def get_all_products():
     return Products
+
+@app.post("/products/mod")
+def update_product(product : Product):
+    for existing_product in Products:
+        if existing_product["ID"] == product.ID:        # if product already exists modify it
+                existing_product["Name"] = product.Name
+                existing_product["Price"] = product.Price
+                existing_product["Type"] = product.Type
+                return f"Modified product"
+
+    new_product = {"ID": product.ID,                    # if product doesn't exist create new product
+                   "Name": product.Name,
+                   "Price": product.Price,
+                   "Type": product.Type
+                   }
+
+    Products.append(new_product)                        # append dictionary
+    return f"Added new product"
+
