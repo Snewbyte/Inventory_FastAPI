1bea1fbb8f348c6a43723ff117f7e3a76292b1cf
Sam <newboldsamuel@gmail.com>
2026-04-05 10:30:57 -0500

    Added price range and search by price and/or type functions to main controller
    added "query" to functions involving SQL statements
    
    added products/prices endpoint in main_controller

--- main_controller.py
+++ main_controller.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from databases import Database
 
 from modules.product_module import Product, ProductRequest
-from services.main_service import get_product_by_id_query, get_all_products_query, convert_data_to_module, insert_new_product, update_product
+from services.main_service import get_product_by_id_query, get_all_products_query, convert_data_to_module, insert_new_product_query, update_product_query, get_products_price_range_query, search_products_query
 from typing import List
 app = FastAPI(title='Module 4 API',
               version="0.0.2",
@@ -22,24 +22,25 @@ async def database_disconnect():
     await database.disconnect()
 
 
-
+##################### POST #####################
 @app.post("/product/mod/")
 async def modify_product(product: ProductRequest):
     results = await database.fetch_all(get_product_by_id_query(product.ID))  # Getting a product by id, will yield at least one if data exist
     message = "" # create outside of if to use
 
     if len(results) > 0: # call the update as we found one
-        await database.execute(update_product(product))  #  We are passing update product query from main_service
+        await database.execute(update_product_query(product))  #  We are passing update product query from main_service
         message = "Successfully updated product"
     else: # nothing found so add new product
-        await database.execute(insert_new_product(product))  #  No data was found, we need to insert new data
+        await database.execute(insert_new_product_query(product))  #  No data was found, we need to insert new data
         message = "Successfully added a new product"
 
     return message
 
 
+##################### GET #####################
 @app.get("/products", response_model=List[Product])
-async def get_product(product_id: str) -> Product:
+async def get_product(product_id: int) -> Product:
     results = await database.fetch_all(get_product_by_id_query(product_id))
 
     # since convert_data_to_module returns a list we just need to return index 0
@@ -48,7 +49,11 @@ async def get_product(product_id: str) -> Product:
 
 @app.get("/product/all", response_model=List[Product])
 async def get_all_products():
-    results = await database.fetch_all(get_all_products_query())
+    results = await database.fetch_all(get_all_products())
 
     return convert_data_to_module(results)
 
+@app.get("/products/prices", response_model=List[Product])
+async def get_products_price_range(min_price: float, max_price: float):
+    results = await database.fetch_all(get_products_price_range_query(min_price, max_price))
+    return convert_data_to_module(results)
--- services/main_service.py
+++ services/main_service.py
@@ -1,17 +1,18 @@
 from modules.product_module import Product, ProductRequest
 
+# keep query in SQL functions so I know calls pertain to SQL queries
 
-### POST ###
+##################### POST #####################
 
 # use single quotes for main return for double quotes inside for valid SQL statements
-def insert_new_product(new_product : ProductRequest):
+def insert_new_product_query(new_product : ProductRequest):
     return f'INSERT INTO PRODUCTS(ID, NAME, PRICE, TYPE) VALUES ({new_product.ID}, "{new_product.Name}", "{new_product.Price}", "{new_product.Type}")'
 
-def update_product(product_data : ProductRequest):
+def update_product_query(product_data : ProductRequest):
     return f'UPDATE PRODUCTS SET NAME="{product_data.Name}", PRICE="{product_data.Price}", TYPE="{product_data.Type}" WHERE ID={product_data.ID}'
 
 
-### GET ###
+##################### GET #####################
 
 def get_product_by_id_query(product_id):
     return f'SELECT * FROM PRODUCTS WHERE ID={product_id}'
@@ -19,6 +20,19 @@ def get_product_by_id_query(product_id):
 
 def get_all_products_query():
     return f'SELECT * FROM PRODUCTS'
+
+def get_products_price_range_query(min_price: float, max_price: float):
+    return f'SELECT * FROM PRODUCTS WHERE PRICE>{min_price} and PRICE<{max_price}'
+
+def search_products_query(price: float = None, type: str = None):
+    if price is not None and type is not None:
+        return f'SELECT * FROM PRODUCTS WHERE PRICE = {price} AND TYPE = {type}'
+    elif price is not None:
+        return f'SELECT * FROM PRODUCTS WHERE PRICE = {price}'
+    elif type is not None:
+        return f'SELECT * FROM PRODUCTS WHERE TYPE = {type}'
+    else:
+        return f'SELECT * FROM PRODUCTS'
 
 
 ### CONVERT ###
