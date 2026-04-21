bf10a512d383999a3976b2b82042c340ce352f83
Sam <newboldsamuel@gmail.com>
2026-04-21 13:09:05 -0500

    Finished unit tests on test_main_controller.py

--- test_main_controller.py
+++ test_main_controller.py
@@ -5,17 +5,14 @@ from main_controller import app
 
 client = TestClient(app)  # creates a duplicate to do testing on
 
-
-
-
 #############   200    #############
 
-# product 200
+# product
 def test_get_product():
-    response = client.get('/product?product_id=1')
+    response = client.get("/product?product_id=1")
     assert response.status_code == 200
 
-# products 200
+# products
 def test_get_all_products():
     response = client.get("/products")
     assert response.status_code == 200
@@ -24,33 +21,56 @@ def test_get_all_products():
     assert product_response[2]["Price"] == 3.99
     assert type(product_response[0]["ID"]) == int
 
-# products/price 200
-def test_product_price():
+# products/price
+def test_product_price_range():
     response = client.get("products/price?min_price=0&max_price=999")
     test = response.json()
+    assert response.status_code == 200
     assert isinstance(test, list)
 
-# products/search 200
-#def test_product_search():
+# products/search
+def test_product_search():
+    response = client.get("/products/search?product_price=1.99&product_type=Vegetable")
+    test = response.json()
+    assert response.status_code == 200
+    assert isinstance(test, list)
+
+def test_product_search_price():
+    response = client.get("/products/search?product_price=1.99")
+    test = response.json()
+    assert response.status_code == 200
+    assert isinstance(test, list)
+
+def test_product_search_type():
+    response = client.get("/products/search?product_type=Vegetable")
+    test = response.json()
+    assert response.status_code == 200
+    assert isinstance(test, list)
+
+# products/mod
+def test_modify_product():
+  response = client.post("/products/mod/", json={"ID": 89,"Name": "Foo Berries","Price": 8.99,"Type": "Fruit"})
+  assert response.status_code == 200
+  assert response.json() == "Successfully updated product"
 
 
 #############   400    #############
 
 # product 400
 def test_product_400():
-    response = client.get('/product?product_id=999')
+    response = client.get("/product?product_id=-1")
     assert response.status_code == 400
-# products 400
+    assert response.json() == {"detail": "Product ID not found"}
 
 # products/price 400
+def test_product_price_400():
+    response = client.get("/products/price?min_price=-5&max_price=-3")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "No products found in that range"}
 
 # products/search 400
+def test_product_search_400():
+    response = client.get("/products/search?product_price=-5&product_type=Nut")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "No products found with that criteria"}
 
-
-
-
-
-def test_yes_product():
-    response = client.get("/product?product_id=3")
-    assert response.status_code == 200
-    assert response.json()["ID"] == 3
