4ee44e8f222594c136b569e3bbd4158f9242f0c6
Sam <newboldsamuel@gmail.com>
2026-04-25 19:43:24 -0500

    added html for post and get caller

--- main_controller.py
+++ main_controller.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.middleware.cors import CORSMiddleware
 from databases import Database
 
 from modules.product_module import Product, ProductRequest, ResponseMessage
@@ -9,8 +10,28 @@ app = FastAPI(title='Module 8 API',
               contact={'name': 'Samuel Newbold', 'email': 'srnewbold17955@mail.mccneb.edu'},
               description='Implementing Unit Tests in FastAPI',)
 
+# setting up middleware that helps validate headers
+#origins = ["*"]
+#app.add_middleware(CORSMiddleware, allow_origins=True, allow_methods=["*"], allow_headers=["*"])
+
 database = Database("sqlite:///services/main.db")
 
+tokens = ["abcdefg"]
+# tokens stored in api and when a user calls your endpoints they pass tokens in the header
+
+# generate token in database -> give to user -> user makes api call -> check for token and increment token counter for that user
+
+# header comes in key value format
+
+# we will make another table that contains a few tokens and keep them in an array
+# check if token is passed through header and check if it is active
+# if it is we return an object
+# if not we return not authorized status code
+# make sure swagger has updated status codes
+
+# "request" object vs request object
+# "request" object is something that is sent by the website to the api and contains information about the sender including headers
+# request object is and object developers define for passing data to the endpoint (ProductRequest in our case)
 
 @app.on_event("startup")
 async def database_connect():
@@ -23,8 +44,8 @@ async def database_disconnect():
 
 
 ##################### POST #####################
-@app.post("/products/mod/", responses={200:{"model": str}, 400: {"model": ResponseMessage}})
-async def modify_product(product: ProductRequest):
+@app.post("/products/mod/", responses={200:{"model": str}, 400: {"model": ResponseMessage}, 401:{"model": ResponseMessage}})
+async def modify_product(product: ProductRequest, request: Request):
     # check empty fields
     if product.Name == "" or product.Type == "":
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Name or type cannot be empty")
@@ -32,6 +53,9 @@ async def modify_product(product: ProductRequest):
     results = await database.fetch_all(get_product_by_id_query(product.ID))  # Getting a product by id, will yield at least one if data exist
     message = "" # create outside of if to use
 
+    print(request.headers["Authorization"]) #if you want to see the full header remove ["Authorization"]
+    if request.headers["Authorization"] not in tokens:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Unauthorized Request")
     if len(results) > 0: # call the update as we found one
         await database.execute(update_product_query(product))  #  We are passing update product query from main_service
         message = "Successfully updated product"
